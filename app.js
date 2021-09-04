44f7a1f0db12197f5fd8ad5d27fae0432c083377
gustavohdev <g_a93@outlook.com>
2021-09-04 08:33:28 -0300

    beautiful css, cached data - both added

--- app.js
+++ app.js
@@ -5,29 +5,45 @@ app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "."));
 const base = require("airtable").base("appNeB87OTy9Ndvlb");
 
+let records;
+
 app.get("/", (req, res) => {
     //your existing code
 
-    (async () => {
-        //...
-        const records = await base("Business Hours")
-            .select({
-                view: "Grid view",
-            })
-            .firstPage();
-
-        for (const record of records) {
-            console.log(record.fields);
-            //console.log(record.get("Day"), record.get("Hours"))
-        }
-        for (const record of records) {
-            // console.log(record.fields)
-            console.log(record.get("Day"), record.get("Hours"));
-        }
+    if (records) {
+        console.log("cached");
         res.render("page", {
             records,
         });
-    })();
+        console.log('running 1')
+    } else {
+        (async () => {
+            //...
+            records = await base("Business Hours")
+                .select({
+                    view: "Grid view",
+                })
+                .firstPage();
+
+            for (const record of records) {
+                ///console.log(record.fields);
+                //console.log(record.get("Day"), record.get("Hours"))
+            }
+            for (const record of records) {
+                // console.log(record.fields)
+                //console.log(record.get("Day"), record.get("Hours"));
+            }
+            res.render("page", {
+                records,
+            });
+
+            setTimeout(() => {
+                records = null;
+            }, 10 * 1000);
+        })();
+
+        console.log('running 2')
+    }
 });
 
 app.listen(3000, () => console.log("Server ready"));
